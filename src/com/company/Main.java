9a59c46d093b1d9813f407d3ac5f1f85eb6eb461
I_Eye <ivan.freeman042@gmail.com>
2022-01-19 07:54:56 -0600

    removes some unnecessary spaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int[] target = new int[2];
         int turnNumber=1;
 
-        System.out.println("Hey, if you don't win this match of Tic-Tac-Toe i'm gonna write Nathan's library on all the pieces of paper in your home");
+        System.out.println("Hey, if you don't win this match of Tic-Tac-Toe i'm gonna replace all the 1997 Ford Fiestas in your house with Nathan's class");
         // a while true loop that takes input from the two players
         // it checks for a win every loop and breaks when it finishes.
         while(true) {
@@ -49,15 +49,14 @@ public class Main {
     }
     // A method that prints the board of the game
     public static void printArray(char[][] gameBoard){
-        for(int i = 0;i<=2;i++){
-            System.out.println("-------------");
-            System.out.println("| "+gameBoard[i][0]+" | "+gameBoard[i][1]+" | "+gameBoard[i][2]+" |");
+        for (char[] chars : gameBoard) {
+            System.out.println("-------------\n" + "| " + chars[0] + " | " + chars[1] + " | " + chars[2] + " |");
         }
         System.out.println("-------------");
     }
     // checks to see if there is a win in all four directions -- vertically, horizontally, and the two diagonals
     public static boolean checkForWin(char[][] gameBoard){
-        for(int i = 0;i<=2;i++){
+        for(int i = 0;i < gameBoard.length;i++){
             if(gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2] && gameBoard[i][0] != '\u0020'){
                 return true;
             } else if(gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i] && gameBoard[0][i] != '\u0020'){
